3be1f0679d17002f0b0e422ce0f6ba833a3ec58c
olafurj <olafurj@olafurj.tern.is>
2017-05-16 15:00:32 +0000

    init

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -o app -I/usr/include/opencv -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_objdetect -lopencv_ocl -lopencv_photo -lopencv_stitching -lopencv_superres -lopencv_ts -lopencv_video -lopencv_videostab
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//#include <opencv/cv.h>
+//#include <opencv/highgui.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main( int argc, char** argv )
+{
+    cv::Mat image;
+    
+    return 0;
+}
+
+
