64ba63283100c15e90419be30a923438ad2692bb
olafurj <olafurj@olafurj.tern.is>
2017-05-18 15:52:43 +0000

    beaf

--- poundpound.cpp
+++ poundpound.cpp
@@ -2,14 +2,19 @@
 #include <cstdlib>
 #include <iostream>
 
+#define r0(p,q,r,s) 0x##p##q##r##s
+
 
 int main() 
 {
 
     unsigned tgtaddr = 123456 & 0x00FFFFFF;
 
-    printf("0x%x\n", tgtaddr);
+    unsigned f = r0(b,e,a,f);
 
+
+    printf("0x%x\n", tgtaddr);
+    printf("0x%x\n", f);
 
     return 0;
 
